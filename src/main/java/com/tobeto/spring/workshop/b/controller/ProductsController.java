1b3340b38d81e4c2b4bcc9b431ee61cf07022c76
Soner <syln.soner@gmail.com>
2023-11-01 18:35:43 +0300

    Postman updated

--- src/main/java/com/tobeto/spring/workshop/b/controller/ProductsController.java
+++ src/main/java/com/tobeto/spring/workshop/b/controller/ProductsController.java
@@ -1,6 +1,9 @@
 package com.tobeto.spring.workshop.b.controller;
+
 import com.tobeto.spring.workshop.b.module.Product;
+
 import jakarta.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -14,44 +17,44 @@ public class ProductsController {
 
     @PostConstruct
     public void initializeProducts() {
-        items.add(new Product(1, "Telefon", 1499.99, 100, "Elektronik"));
-        items.add(new Product(2, "Televizyon", 2599.99, 50, "Elektronik"));
-        items.add(new Product(3, "Kitap", 19.99, 200, "Kitap"));
-        items.add(new Product(4, "Giyim", 49.99, 150, "Moda"));
-        items.add(new Product(5, "Saat", 299.99, 80, "Aksesuar"));
-        items.add(new Product(6, "Ayakkabı", 79.99, 120, "Ayakkabı"));
-        items.add(new Product(7, "Masa", 199.99, 30, "Mobilya"));
-        items.add(new Product(8, "Kamera", 899.99, 40, "Elektronik"));
-        items.add(new Product(9, "Yemek Takımı", 149.99, 60, "Ev Eşyaları"));
-        items.add(new Product(10, "Parfüm", 39.99, 90, "Kozmetik"));
+        items.add(new Product(1, "Phone", 1499.99, 100, "Electronics"));
+        items.add(new Product(2, "TV", 2599.99, 50, "Electronics"));
+        items.add(new Product(3, "Book", 19.99, 200, "Books"));
+        items.add(new Product(4, "Clothing", 49.99, 150, "Fashion"));
+        items.add(new Product(5, "Watch", 299.99, 80, "Accessories"));
+        items.add(new Product(6, "Shoes", 79.99, 120, "Footwear"));
+        items.add(new Product(7, "Table", 199.99, 30, "Furniture"));
+        items.add(new Product(8, "Camera", 899.99, 40, "Electronics"));
+        items.add(new Product(9, "Dining Set", 149.99, 60, "Home Decor"));
+        items.add(new Product(10, "Perfume", 39.99, 90, "Cosmetics"));
     }
 
     @GetMapping("/getall")
-    public List<Product> getAllProductsSortedById(){
+    public List<Product> getAllProductsSortedById() {
         return items.stream()
                 .sorted(Comparator.comparingInt(Product::getId))
                 .collect(Collectors.toList());
     }
 
     @PostMapping("/add")
-    public String createProduct(@RequestBody Product product){
+    public String createProduct(@RequestBody Product product) {
         this.items.add(product);
-        return product.getName() + " adlı ürün başarıyla eklendi.";
+        return product.getName() + " product added successfully.";
     }
 
     @PutMapping("/update/{id}")
-    public String updateProduct (@PathVariable int id, @RequestBody Product updateProduct){
+    public String updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
         for (Product product : items) {
-            if (product.getId() == id){
-                product.setName(updateProduct.getName());
-                product.setCategory(updateProduct.getCategory());
-                product.setStock(updateProduct.getStock());
-                product.setPrice(updateProduct.getPrice());
+            if (product.getId() == id) {
+                product.setName(updatedProduct.getName());
+                product.setCategory(updatedProduct.getCategory());
+                product.setStock(updatedProduct.getStock());
+                product.setPrice(updatedProduct.getPrice());
 
-                return product.getName() + " adlı ürün başarıyla güncellendi!";
+                return product.getName() + " product updated successfully!";
             }
         }
-        return "Ürün bulunamadı!";
+        return "Product not found!";
     }
 
     @DeleteMapping("/delete/{id}")
@@ -59,9 +62,9 @@ public class ProductsController {
         for (Product product : items) {
             if (product.getId() == id) {
                 items.remove(product);
-                return product.getName() + " adlı ürün başarıyla silindi.";
+                return product.getName() + " product deleted successfully.";
             }
         }
-        return "Ürün bulunamadı!";
+        return "Product not found!";
     }
 }
--- src/main/java/com/tobeto/spring/workshop/b/controller/UsersController.java
+++ src/main/java/com/tobeto/spring/workshop/b/controller/UsersController.java
@@ -1,6 +1,9 @@
 package com.tobeto.spring.workshop.b.controller;
+
 import com.tobeto.spring.workshop.b.module.User;
+
 import jakarta.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -13,18 +16,19 @@ public class UsersController {
     List<User> items = new ArrayList<>();
 
     @PostConstruct
-    public void initializeUsers(){
-        items.add(new User(1,"Ahmet","ahmet@ahmet.com","ahmet123","5343572837"));
-        items.add(new User(2,"Süleyman","suleyman@suleyman.com","suleyman123","5333572837"));
-        items.add(new User(3,"Şeyhmus","seyhmus@seyhmus.com","seyhmus123","5343682837"));
-        items.add(new User(4, "Mustafa", "mustafa@example.com", "mustafa123", "55551234"));
-        items.add(new User(5, "Emine", "emine@example.com", "emine123", "55552345"));
-        items.add(new User(6, "Zeynep", "zeynep@example.com", "zeynep123", "55563456"));
-        items.add(new User(7, "Mehmet", "mehmet@example.com", "mehmet123", "55574567"));
-        items.add(new User(8, "Ayşe", "ayse@example.com", "ayse123", "55585678"));
-        items.add(new User(9, "Fırat", "firat@example.com", "firat123", "55596789"));
-        items.add(new User(10, "Gül", "gul@example.com", "gul123", "55501010"));
+    public void initializeUsers() {
+        items.add(new User(1, "John", "john@john.com", "john123", "5343572837"));
+        items.add(new User(2, "Michael", "michael@michael.com", "michael123", "5333572837"));
+        items.add(new User(3, "Sophia", "sophia@sophia.com", "sophia123", "5343682837"));
+        items.add(new User(4, "Emma", "emma@emma.com", "emma123", "55551234"));
+        items.add(new User(5, "Oliver", "oliver@oliver.com", "oliver123", "55552345"));
+        items.add(new User(6, "Ava", "ava@ava.com", "ava123", "55563456"));
+        items.add(new User(7, "Liam", "liam@liam.com", "liam123", "55574567"));
+        items.add(new User(8, "Isabella", "isabella@isabella.com", "isabella123", "55585678"));
+        items.add(new User(9, "Sophie", "sophie@sophie.com", "sophie123", "55596789"));
+        items.add(new User(10, "Lucas", "lucas@lucas.com", "lucas123", "55501010"));
     }
+
 
     @GetMapping("/getall")
     public List<User> getAllUsersSortedById() {
@@ -32,14 +36,15 @@ public class UsersController {
                 .sorted(Comparator.comparingInt(User::getId))
                 .collect(Collectors.toList());
     }
+
     @PostMapping("/add")
-    public String createUser(@RequestBody User user){
+    public String createUser(@RequestBody User user) {
         this.items.add(user);
-        return user.getName() + " kullanıcı başarıyla eklendi.";
+        return user.getName() + " user added successfully.";
     }
 
     @PutMapping("/update/{id}")
-    public String updateUser (@PathVariable int id, @RequestBody User updatedUser){
+    public String updateUser(@PathVariable int id, @RequestBody User updatedUser) {
         for (User user : items) {
             if (user.getId() == id) {
                 user.setName(updatedUser.getName());
@@ -47,12 +52,11 @@ public class UsersController {
                 user.setPassword(updatedUser.getPassword());
                 user.setPhone(updatedUser.getPhone());
 
-                return "Kullanıcı güncellendi! \nID: " + id + ", \nYeni Bilgiler: \nİsim: " + user.getName() +
-                        ", \nE-mail: " + user.getEmail() + ", \nŞifre: " + user.getPassword() + "\nTelefon: " + user.getPhone();
+                return "User updated! \nID: " + id + ", \nNew Information: \nName: " + user.getName() +
+                        ", \nEmail: " + user.getEmail() + ", \nPassword: " + user.getPassword() + "\nPhone: " + user.getPhone();
             }
-
         }
-        return  "Kullanıcı bulunamadı!";
+        return "User not found!";
     }
 
     @DeleteMapping("/delete/{id}")
@@ -60,11 +64,9 @@ public class UsersController {
         for (User user : items) {
             if (user.getId() == id) {
                 items.remove(user);
-                return user.getName() + " kullanıcı başarıyla silindi.";
+                return user.getName() + " user deleted successfully.";
             }
         }
-
-        return "Kullanıcı bulunamadı.";
+        return "User not found.";
     }
-
 }
